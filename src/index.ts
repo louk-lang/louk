9d1baef62b1715b4a5ff815cf062ca46b198b078
agorischek <alex.gorischek@gmail.com>
2018-08-25 12:31:00 -0700

    Add src folder, switch to TypeScript files

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,12 @@
 const _ = require("underscore")
 const fs = require('fs');
-var log = false
+let log = false
 
 require.extensions['.louk'] = function (module, filename) {
     module.exports = fs.readFileSync(filename, 'utf8');
 };
 
-const patterns = {
-
-
-        //PREFIXES
-        //Prefixes are nonalphabetic modifiers that precede the key.
-
-        //All valid prefixes: ~ and : and @ and -
-        //Shorthands like . and # are not prefixes, they are cruxes.
-        prefix: /^([~:@-])/,
-
-        //Prefixes that can make an attribute static: ~
-        staticPrefix: /^([~])/,
-
-
-        //SUFFIXES
-        //Suffixes are nonalphabetic modifiers that follow the key.
-
-        //All valid suffixes: ~ and /
-        suffix: /([~/])$/,
-
-        //Suffixes that can make an element static: ~ and /
-        // The forward slash makes an element self-closing, and therefore not capable of containing dynamic content.
-        staticSuffix: /([~/])$/,
-
-
-        //CRUXES
-        //The crux is the string present in the source Louk that indicates what the line represents.
-        //It is most commonly the same as the content before the first space or new line.
-        //However, shorthands like "#" and "." are important exceptions.
-
-        //Cruxes not followed by content, such as "a"
-        plainCrux: /^(.+)/,
-
-        //Cruxes that are followed by content, such as "a b"
-        modifiedCrux: /^(.+?)\s/,
-
-        //Shorthand cruxes that make their attribute static: > and # and .
-        //The first capture group gets the shorthand crux, the second capture group gets the fill.
-        staticCrux: /^([>#\.]).*/,
-
-
-        //FILLS
-        //The fill is the "stuff" of the line: It's the content inside the element or the value of the attribute.
-
-        //A normal fill, preceded by a space
-        fill: /^.+?\s(.+)/,
-
-        //A fill prepended by a static crux
-        staticFill: /^[>#\.](.*)/,
-
-
-        //KEYS
-        //A key is semantically what a line of Louk ultimately represents: A specific tag or a specific attribute.
-        //A key might be implied or it might have a shorthand. For example, "." is a crux, and "class" is its key.
-
-        key: /^[~:@-]*([\w\.-]+)/,
-
-
-        //OTHER
-
-        //Characters that indicate the line should be interpretted as a comment.
-        //The capture group captures the comment.
-        comment: /^\/\/(.*)/,
-
-        //Characters that indicate the line should be interpretted as HTML
-        html: /^([<])/,
-
-        //Used to identify whether we've hit the first non-space character of a line yet.
-        initialSpace: /^(\s)/
-}
-
+const patterns = require("./patterns")
 
 module.exports = function(content){
     return html = parse(content);

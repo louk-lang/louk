622bc68f5da771563a355c279a1d68369317150c
agorischek <alex.gorischek@gmail.com>
2018-10-03 23:08:27 -0700

    Bump package version

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 const parser = require("./parser")
 
-// Turn verbose logging on or off
-let logging = false
-
 module.exports = function(content, options){
     return parser.parse(content, options, logging);
 }

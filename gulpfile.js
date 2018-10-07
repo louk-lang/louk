8a506c1d747eefbf6c9c761fa75e88950c2cf5eb
agorischek <alex.gorischek@gmail.com>
2018-10-06 22:40:10 -0700

    Update to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,24 @@ var mocha = require('gulp-mocha');
 var tsconfig = require('./tsconfig.json');
 var outDir = tsconfig.compilerOptions.outDir;
 
+var sourceGlob = 'src/**/*.ts';
+var testGlob = './test/**/*.js';
+
 var tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('build', function() {
-    return gulp.src('src/**/*.ts')
+    return gulp.src(sourceGlob)
         .pipe(tsProject())
         .pipe(gulp.dest(outDir));
 });
 
 gulp.task('test', function(){
-    return gulp.src('./test/test.js', {read: false})
+    return gulp.src(testGlob, {read: false})
         .pipe(mocha());
 });
 
-gulp.task('watch', ['default'], function() {
-    return gulp.watch('src/*.ts', ['default']);
-});
+gulp.task('default', gulp.series('build', 'test'));
 
-gulp.task('default', ['build', 'test'], function(){
-
-});
+gulp.task('watch', gulp.series('default', function() {
+    return gulp.watch([sourceGlob, testGlob], gulp.series('default'));
+}));

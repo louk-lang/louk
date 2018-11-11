6fbcbb188eeaf78ebc8ca63150efa60fce3c9ef2
agorischek <alex.gorischek@gmail.com>
2018-11-11 09:14:44 -0800

    Add initial multiline tests

---
+++ test/multiline.js
@@ -0,0 +1,19 @@
+var config = require("./config.js");
+var file = config.file;
+var assert = config.assert;
+
+var louk = require(file("index.js"));
+
+var fs = require('fs');
+
+describe("Multiline", function(){
+    it("should return a two-line element", function(){
+        assert.equal(louk('a b\n| c'),'<a>{{b}}{{c}}</a>');
+    });
+    it("should return a three-line element", function(){
+        assert.equal(louk('a b\n| c\n| d'),'<a>{{b}}{{c}}{{d}}</a>');
+    });
+    it("should return two two-line elements", function(){
+        assert.equal(louk('a b\n| c\nd e\n| f'),'<a>{{b}}{{c}}</a><d>{{e}}{{f}}</d>');
+    });
+});

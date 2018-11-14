0c36ebebae4ed129d8e180555d535c5761042992
agorischek <alex.gorischek@gmail.com>
2018-11-13 21:35:32 -0800

    Remove test logging

--- test/multiline.js
+++ test/multiline.js
@@ -21,28 +21,24 @@ describe("Multiline", function(){
         assert.equal(louk('a b\n| c\nd e',{whitespace:false}),'<a>{{b}}{{c}}</a><d>{{e}}</d>');
     });
     it("should return a three-line element with whitespace", function(){
-        // console.log("<a>{{b}}{{c}}{{d}}</a>")
         assert.equal(louk('a b\n| c\n| d',{whitespace:true}),'<a>{{b}}{{c}}{{d}}</a>');
     });
     it("should return a three-line element without whitespace", function(){
         assert.equal(louk('a b\n| c\n| d',{whitespace:false}),'<a>{{b}}{{c}}{{d}}</a>');
     });
     it("should return two two-line elements with whitespace", function(){
-        // console.log("<a>{{b}}{{c}}</a>\n<d>{{e}}{{f}}</d>")
         assert.equal(louk('a b\n| c\nd e\n| f',{whitespace:true}),'<a>{{b}}{{c}}</a>\n<d>{{e}}{{f}}</d>');
     });
     it("should return two two-line elements without whitespace", function(){
         assert.equal(louk('a b\n| c\nd e\n| f',{whitespace:false}),'<a>{{b}}{{c}}</a><d>{{e}}{{f}}</d>');
     });
     it("should disregard attributes following continuations with whitespace", function(){
-        // console.log("<a>{{c}}</a>");
         assert.equal(louk('a\n| c\n#id',{whitespace:true}),'<a>{{c}}</a>');
     });
     it("should disregard attributes following continuations without whitespace", function(){
         assert.equal(louk('a\n| c\n#id',{whitespace:false}),'<a>{{c}}</a>');
     });
     it("should return a multiline element with a nested element with whitespace", function(){
-        // console.log("<a>\n\t{{b}}\n\t<c>{{d}}</c>\n\t{{e}}\n</a>");
         assert.equal(louk('a b\n\tc d\n| e',{whitespace:true}),'<a>\n\t{{b}}\n\t<c>{{d}}</c>\n\t{{e}}\n</a>');
     });
     it("should return a multiline element with a nested element without whitespace", function(){
@@ -56,14 +52,12 @@ describe("Multiline", function(){
         assert.equal(louk('a b\n\tc d\n\t| e\n| f',{whitespace:false}),'<a>{{b}}<c>{{d}}{{e}}</c>{{f}}</a>');
     });
     it("should return a multiline element with two nested elements with whitespace", function(){
-        // console.log("<a>\n\t{{b}}\n\t<c>{{d}}{{e}}</c>\n\t<f>{{g}}</f>\n\t{{h}}\n</a>")
         assert.equal(louk('a b\n\tc d\n\t| e\n\tf g\n| h',{whitespace:true}),'<a>\n\t{{b}}\n\t<c>{{d}}{{e}}</c>\n\t<f>{{g}}</f>\n\t{{h}}\n</a>');
     });
     it("should return a multiline element with two nested elements without whitespace", function(){
         assert.equal(louk('a b\n\tc d\n\t| e\n\tf g\n| h',{whitespace:false}),'<a>{{b}}<c>{{d}}{{e}}</c><f>{{g}}</f>{{h}}</a>');
     });
     it("should handle multiline with multiple outdents with whitespace", function(){
-        // console.log("<a>\n\t<b>\n\t\t<c></c>\n\t</b>\n\t{{d}}\n</a>");
         assert.equal(louk('a\n\tb\n\t\tc\n| d',{whitespace:true}),'<a>\n\t<b>\n\t\t<c></c>\n\t</b>\n\t{{d}}\n</a>');
     });
     it("should handle multiline with multiple outdents without whitespace", function(){
